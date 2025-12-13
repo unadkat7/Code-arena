9791ff1ffb41e4ebd2781e3c9e0ba7d4d2f2f444
unadkat7 <153261041+unadkat7@users.noreply.github.com>
2025-12-13 18:43:37 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,29 @@
 import express from "express";
+import path from "path"
 import { ENV } from "./lib/env.js";
+
+
 const app = express();
 
+const __dirname = path.resolve();
 
-
-app.get("/",(req,res)=>{
-    res.status(200).json({msg:"success from backend123"})
+app.get("/health",(req,res)=>{
+    res.status(200).json({msg:"api is up and running!"})
 })
+
+app.get("/books",(req,res)=>{
+    res.status(200).json({msg:"booooookkkksss!"})
+})
+
+//Make your app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+}
+
+
 
 
 app.listen(ENV.PORT,()=> console.log("Server started on port ",ENV.PORT ));
